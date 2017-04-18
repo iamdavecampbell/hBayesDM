ba2be4b9601ab7793cf73ccb7bbb27f072b52321
Dave Campbell <iamdavecampbell@users.noreply.github.com>
2017-04-17 19:04:29 -0700

    Update zzz.R
    debugging

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,8 @@
 .onLoad <- function(libname, pkgname) {
   modules <- paste0("stan_fit4", names(stanmodels), "_mod")
   print(modules)
-  print(class(modules))
-  print(attributes(modules[1]))
-  for (m in modules) loadModule(m, what = TRUE)
+  for (m in modules){
+   print(m)
+   loadModule(m, what = TRUE)
+   }
 }
